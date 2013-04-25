40688f2289da82ff25a592915c832d3f99cf10f9
greatghoul <greatghoul@gmail.com>
2013-04-26 01:43:45 +0800

    Update deploy script

--- deploy.bat
+++ deploy.bat
@@ -1,13 +1,3 @@
 @echo off
-title Deploying resume files ...
-git branch -D gh-pages
-git checkout --orphan gh-pages
-git status
-git rm -r -q --cached .
-for /f %%G in ('dir /b .') do ( if not "%%G" == "resume" del /S /F /Q %%G )
-move resume\* . 
-rd /S /Q resume
-git status
-git add .
-git commit -m "First Commit"
-
+ghp-import resume
+pause
