facd9a30af0c2cb4a1d7b49dc3fbbc4857723e3d
greatghoul <greatghoul@gmail.com>
2013-04-26 00:09:53 +0800

    d

--- deploy.bat
+++ deploy.bat
@@ -2,8 +2,11 @@ title Deploying resume files ...
 git branch -D gh-pages
 git checkout --orphan gh-pages
 
-move .git resume
+xcopy /E /H /R /K /Y /I .git resume\.git
+rd /S /Q .git
+
 cd resume
 git add .
 git commit -m "First Commit"
+
 
