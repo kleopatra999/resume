14a046757e56e9f040316f99701b1a6c31acf5e9
greatghoul <greatghoul@gmail.com>
2013-04-25 22:18:28 +0800

    更新bat

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,11 @@
 @echo off
 title Deploying resume files ...
-git checkout -B gh-pages
-rd /S /Q .git
+git branch -d -f gh-pages
+git checkout --orphan gh-pages
+move .git resume\.git
+move resume %TEMP%\resume
+del /F /S *
+move %TEMP%\resume\* .\  
 git add .
 git commit -m "First Commit"
 git push --force origin gh-pages
