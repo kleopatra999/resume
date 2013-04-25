0a0d088970fcec9cad5f8d58699af5a208d93118
greatghoul <greatghoul@gmail.com>
2013-04-25 21:41:36 +0800

    更新 windows 的上传脚本

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,10 @@
 @echo off
 title Deploying resume files ...
-ghp-import.py -p resume
+git checkout gh-pages
+rd /S /Q .git
+git add .
+git commit -m "First Commit"
+git push --force origin gh-pages
+git checkout master
+echo Everything is done.
 pause
