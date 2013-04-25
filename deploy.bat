cb7e8fd967a592675a41a95ac7b064e1bf9aac9f
greatghoul <greatghoul@gmail.com>
2013-04-26 00:32:09 +0800

    D

--- deploy.bat
+++ deploy.bat
@@ -2,7 +2,7 @@ title Deploying resume files ...
 git branch -D gh-pages
 git checkout --orphan gh-pages
 git rm -r -q --cached .
-
+git status
 xcopy /E /H /R /K /Y /I .git resume\.git
 rd /S /Q .git
 
