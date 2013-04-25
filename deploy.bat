b37d89b91d52f72ae3abbb2e23d20a2244306b00
greatghoul <greatghoul@gmail.com>
2013-04-26 01:09:01 +0800

    fdsa

--- deploy.bat
+++ deploy.bat
@@ -1,12 +1,12 @@
+@echo off
 title Deploying resume files ...
 git branch -D gh-pages
 git checkout --orphan gh-pages
-git rm -r -q --cached .
 git status
-xcopy /E /H /R /K /Y /I .git resume\.git
-rd /S /Q .git
-
-cd resume
+git rm -r -q --cached .
+for /f %%G in ('dir /b .') do ( if not "%%G" == "resume" del /S /F /Q %%G )
+move resume\* . 
+rd /S /Q resume
 git status
 git add .
 git commit -m "First Commit"
